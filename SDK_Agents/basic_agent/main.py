bec0cf657e0992c5932ff80a8d564a358c0a41a6
Muhammad-Tayyab <160000753+Moh-Tayyab@users.noreply.github.com>
2025-06-23 20:27:10 +0500

    updation

--- SDK_Agents/basic_agent/main.py
+++ SDK_Agents/basic_agent/main.py
@@ -1,27 +1,36 @@
-from agents import Agent, Runner
-from openai import AsyncOpenAI
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent, Runner, OpenAIChatCompletionsModel
+#import google.generativeai as genai
 
 load_dotenv()
 
-GOOGLE_KEY = os.getenv("GOOGLE_API_KEY")
+def main():
+    gemini_api_key = os.getenv('GEMINI_API_KEY')
+    
+    client = AsyncOpenAI(
+        api_key = gemini_api_key,
+         base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    )
+    
+    agent = Agent(
+        name = 'Assistant',
+        instructions = "You are expert agent",
+        model = OpenAIChatCompletionsModel(
+            model = 'gemini-2.0-flash',
+            openai_client = client,
+            )
+    )
+    
+    query = input("Enter your query: ")
+    
+    result = Runner.run_sync(
+        agent, 
+        query
+    )
+    
+    print(result.final_output)
 
-client = AsyncOpenAI(
-    api_key=GOOGLE_KEY,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/ "
-)
-
-agent = Agent(
-    name = "basic_agent",
-    instructions = "You are a helpful assistant. Answer's question to the best of your ability"
-)
-
-query = "what is the capital of UK?"
-
-result = Runner.run_sync(
-    agent,
-    query 
-)
-
-print(result.final_output)
+if __name__ == "__main__":
+    main()
